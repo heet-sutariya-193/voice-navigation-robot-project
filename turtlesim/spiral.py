8d080ab0dbc33e1b8b8da6418faf93f1de57fc66
heet-sutariya-193 <heetsutariya107@gmail.com>
2024-07-03 14:30:04 +0530

    Update spiral.py

--- turtlesim/spiral.py
+++ turtlesim/spiral.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python3
 
+import rospy
+from geometry_msgs.msg import Twist
+from math import pi
+if _name=='main_':
+    rospy.init_node("spiral")
+    rospy.loginfo("node started")
+
+    pub=rospy.Publisher("/turtle1/cmd_vel",Twist,queue_size=10)
+
+    rate=rospy.Rate(1)
+    l=0.5
+    a=5
+
+    while not rospy.is_shutdown():
+        mes=Twist()
+        mes.linear.x=l
+        mes.angular.z=a
+        l+=0.5
+        a-=0.5
+
+        pub.publish(mes)
+        rate.sleep()
